59f95a29a10d15be25c8d76361d3857c2e9cb8d9
Muhammadjon Tohirov <tokhirov.mukhammadjon@gmail.com>
2025-09-23 12:29:12 +0500

    bgfix

--- Sources/MapPack/MapProviders/Libre/MapLibreWrapperModel+Marker.swift
+++ Sources/MapPack/MapProviders/Libre/MapLibreWrapperModel+Marker.swift
@@ -12,7 +12,6 @@ import MapLibre
 
 extension MapLibreWrapperModel {
     func updateMarker(_ marker: any UniversalMapMarkerProtocol) {
-        Logging.l("Update marker")
         let annotation = self.markers[marker.id]
         UIView.animate(withDuration: 1, delay: 0, options: .curveLinear) {
             annotation?.set(coordinate: marker.coordinate)
