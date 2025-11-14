560430a07dd9489d6f66b8e5e777afc56fc9803c
Muhammadjon Tohirov <tokhirov.mukhammadjon@gmail.com>
2025-11-14 16:18:19 +0500

    Custom map style

--- Sources/MapPack/Models/UniversalMapStyles.swift
+++ Sources/MapPack/Models/UniversalMapStyles.swift
@@ -17,10 +17,22 @@ public struct GoogleLightMapStyle: UniversalMapStyleProtocol {
     public var source: String {
         GMapStyles.default
     }
+    
+    public init() {}
 }
 
 public struct GoogleDarkMapStyle: UniversalMapStyleProtocol {
     public var source: String {
         GMapStyles.dark
+    }
+    
+    public init() {}
+}
+
+public struct CustomMapStyle: UniversalMapStyleProtocol {
+    public var source: String
+    
+    public init(source: String) {
+        self.source = source
     }
 }
