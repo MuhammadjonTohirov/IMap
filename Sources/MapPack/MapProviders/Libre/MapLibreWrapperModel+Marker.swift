4409241154819e9189381ba5d199f3d97f83c422
Muhammadjon Tohirov <tokhirov.mukhammadjon@gmail.com>
2026-01-21 10:43:33 +0500

    updateMarker works if there is change on marker

--- Sources/MapPack/MapProviders/Libre/MapLibreWrapperModel+Marker.swift
+++ Sources/MapPack/MapProviders/Libre/MapLibreWrapperModel+Marker.swift
@@ -13,9 +13,28 @@ import MapLibre
 extension MapLibreWrapperModel {
     func updateMarker(_ marker: any UniversalMapMarkerProtocol) {
         let annotation = self.markers[marker.id]
+        var hasChange: Bool = true
+        
+        if let annotation {
+            hasChange = annotation.coordinate != marker.coordinate || annotation.rotation != marker.rotation
+        }
+        
+        guard hasChange else {
+            return
+        }
+        
         UIView.animate(withDuration: 1, delay: 0, options: .curveLinear) {
             annotation?.updatePosition(coordinate: marker.coordinate, heading: marker.rotation)
             annotation?.view?.transform = CGAffineTransform(rotationAngle: (.pi / 180) * CGFloat(marker.rotation))
         }
+    }
+}
+
+extension CLLocationCoordinate2D: @retroactive Equatable {
+    public static func ==(lhs: CLLocationCoordinate2D, rhs: CLLocationCoordinate2D) -> Bool {
+        if lhs.latitude == rhs.latitude && lhs.longitude == rhs.longitude {
+            return true
+        }
+        return false
     }
 }
